3c6e62ac331bdb160e179392f44335f553c78669
Leonardo Vasconcelos <leovasc5@hotmail.com>
2021-06-03 15:54:16 -0300

    Atualizado sistema de horário. Incluído icone do site. Começarei a planejar a incluir a API do climatempo

--- js/main.js
+++ js/main.js
@@ -1,36 +1,39 @@
-setTimeout(function() {
-    location.reload();
-  }, 30000);
+setInterval(function() {
+    carregar();
+  }, 1000);
 
 function carregar(){
-    var img = document.getElementById('#img');
+    var img = document.getElementById('img');
     var msg = document.querySelector('#msg');
     var body = document.getElementById('body');
     var titulo = document.getElementById('tit');
 
     var data = new Date();
     var hora = data.getHours();
-    msg.innerText = 'Agora são: ' + hora +' horas';
+    var minuto = data.getMinutes();
+    var segundo = data.getSeconds();
+    msg.innerText = hora +':' + minuto + ':' + segundo;
 
     if((hora >= 0) && (hora < 6)){
-        img.src = '../img/madrugada.png';
+        img.src = 'img/madrugada.png';
         body.style.backgroundColor = '#353839';
         titulo.innerText = 'Boa madrugada!';
         titulo.style.color = '#f4f4f4';
     }else if((hora >= 6) && (hora < 12)){
-        img.src = '../img/manha.png';
+        img.src = 'img/manha.png';
         body.style.backgroundColor = '#87CEFA';
         titulo.innerText = 'Boa dia!';
     }else if((hora >= 12) && (hora < 18)){
-        img.src = '../img/tarde.png';
+        img.src = 'img/tarde.png';
         body.style.backgroundColor = '#00BFFF';
         titulo.innerText = 'Boa tarde!';
     }else if((hora >= 18) && (hora < 24)){
-        img.src = '../img/noite.png';
+        img.src = 'img/noite.png';
         body.style.backgroundColor = '#080C2C';
         titulo.innerText = 'Boa noite!';
         titulo.style.color = '#f4f4f4';
     }
+
 }
 
 /*Criado por Leonardo Vasconcelos Paulino 
